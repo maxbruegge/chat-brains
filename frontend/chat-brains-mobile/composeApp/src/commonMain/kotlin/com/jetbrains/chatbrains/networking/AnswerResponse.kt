bfc54f340e50fa2de2eb1323778d11a38834934e
Max Rosenblattl <max.rosenblattl@tum.de>
2024-11-24 00:29:04 +0100

    edit answer response

--- frontend/chat-brains-mobile/composeApp/src/commonMain/kotlin/com/jetbrains/chatbrains/networking/AnswerResponse.kt
+++ frontend/chat-brains-mobile/composeApp/src/commonMain/kotlin/com/jetbrains/chatbrains/networking/AnswerResponse.kt
@@ -1,4 +1,4 @@
 package com.jetbrains.chatbrains.networking
 
 @kotlinx.serialization.Serializable
-data class AnswerResponse(val success: Boolean, val answer: String)
+data class AnswerResponse(val success: Boolean, val answer: String, val file: String)
